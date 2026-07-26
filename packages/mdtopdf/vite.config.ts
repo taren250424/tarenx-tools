04c986e2abf3302325aad30a5cf6dbb3a89c283c
taren250424 <taren250424@gmail.com>
2026-07-26 16:32:53 +0900

    feat(mdtopdf): add new util mdtopdf

---
+++ packages/mdtopdf/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, normalizePath } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
+
+export default defineConfig({
+  base: '/mdtopdf/',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(path.resolve(__dirname, '../shared/mdtopdf')) + '/**/*',
+          dest: 'shared',
+          rename: { stripBase: 1 },
+        }
+      ]
+    })
+  ],
+})
